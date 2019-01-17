eb5545d966242a21b5c8390d576df56ecd9e4bbd
phatdoan <doanminhphat0212@gmail.com>
2019-01-17 13:45:40 +0200

    uploading http-b

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+import { PhotoViewer } from "@ionic-native/photo-viewer";
+import { HttpClient } from '@angular/common/http';
+
+import { Pic } from '../../interfaces/pic';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+export class HomePage implements OnInit{
+  url = 'http://media.mw.metropolia.fi/wbma/uploads/';
+  picArray: Pic[] = [];
+  constructor(
+    public navCtrl: NavController,
+    public photoViewer: PhotoViewer,
+    public http: HttpClient,
+  ) {}
+  viewImage(imageUrl: string) {
+    this.photoViewer.show(imageUrl);
   }
-
+  ngOnInit() {
+    this.http.get('http://media.mw.metropolia.fi/wbma/media/').subscribe((res: Pic[]) => {
+      this.picArray = res;
+      console.log(res);
+    });
+  }
 }
